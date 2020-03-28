1583ed10c95e77b60ffd5edb75b9c3fc27ff29c4
gtkChop <swaroopbhatk@gmail.com>
2020-03-28 22:52:32 +0000

    added uwsgit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from covid19viz.toolkit import register
 
 
-if __name__ == "__main__":
-    app_instance = register.RegisterDashApplication()
-    app = app_instance.app
-    app.run_server(debug=False)
+app_instance = register.RegisterDashApplication()
+app = app_instance.app
+app.run_server(debug=False)
