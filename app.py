94a37e7e8028302a3f60039cbb93d0958f72fe74
gtkChop <swaroopbhatk@gmail.com>
2020-03-29 20:05:06 +0100

    changed port to 0.0.0.0

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from covid19viz.toolkit import register
 
 app_instance = register.RegisterDashApplication()
 app = app_instance.app
-app.run_server(debug=False)
+app.run_server(host="0.0.0.0", debug=False)
